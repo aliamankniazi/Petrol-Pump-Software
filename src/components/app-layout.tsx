a3fca2833d2705f5ab26918314ca129f48d814e6
محمد ضیاء اللہ خان نیازی <ziakniazi@gmail.com>
2025-07-18 18:43:29 +0000

    Add Mianwali Petroleum Service In Monogram Also

--- src/components/app-layout.tsx
+++ src/components/app-layout.tsx
@@ -50,7 +50,7 @@ export function AppLayout({ children }: { children: React.ReactNode }) {
         <SidebarHeader className='p-4'>
           <div className="flex items-center gap-2">
             <ShellLogo className="text-primary w-8 h-8" />
-            <h1 className="text-xl font-bold">Mianwali Petroleum</h1>
+            <h1 className="text-lg font-semibold">Mianwali Petroleum Service</h1>
           </div>
         </SidebarHeader>
         <SidebarContent>
